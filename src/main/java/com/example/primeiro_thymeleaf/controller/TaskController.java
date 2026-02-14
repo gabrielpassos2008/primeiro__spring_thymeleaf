ce6e4c85892afd9acf1af7ea41cb7f47d8152155
Gabrielapassos2008 <gabrielpassos2008@email.com>
2026-02-14 01:08:41 -0300

    -criei o arquivo html que iria exibir a lista. -criei metedo getMapping e enviei um objeto usando o modelAndView.

--- src/main/java/com/example/primeiro_thymeleaf/controller/TaskController.java
+++ src/main/java/com/example/primeiro_thymeleaf/controller/TaskController.java
@@ -6,6 +6,7 @@ import java.util.List;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 import com.example.primeiro_thymeleaf.model.Task;
 
@@ -20,13 +21,16 @@ public class TaskController {
     }
 
     @PostMapping("/create")
-    public void postCreate(Task task) {
+    public String postCreate(Task task) {
         Long id = tasks.size() + 1L;
         tasks.add(new Task(id, task.getName(), task.getDate()));
+        return "redirect:/list";
     }
 
-    @GetMapping("/lista")
-    public String getList() {
-        return "list";
+    @GetMapping("/list")
+    public ModelAndView getList() {
+        ModelAndView mv = new ModelAndView("list");
+        mv.addObject("tasks", tasks);
+        return mv;
     }
 }
